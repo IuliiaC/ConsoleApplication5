bf06b6c92a6410d665ae37a7fa96f28a9c8f7243
Iuliia Chmykhun <Iuliia_Chmykhun@epam.com>
2018-04-27 16:44:34 +0300

    Done

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main()
+        {
+            var fruitArray = new string[] { "apple", "avocado", "banana", "apricot", "jackfruit", "cherimoya", "papaya", "pineapple", "lime", "lemon", "grape", "orange", "grapefruit", "strawberry"};
+            string findap = "ap";
+            string symbolA = "a";
+            string symbolB = "p";
+
+
+
+            for (int i = 0; i < fruitArray.Length; i++)
+            {
+                string m = fruitArray[i];
+               bool b = m.Contains(findap);
+                if (b)
+                {
+                    Console.WriteLine("подходит " + fruitArray[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
